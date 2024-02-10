9930211c4dc68e3a3e5b035c3deaa5ef33714168
Kent Edoloverio <kentedoloverio25@gmail.com>
2024-02-10 16:58:22 +0800

    Adding API feature

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, jsonify
+from flask_restful import Api, Resource, reqparse
+import random
+import os
+
 
 app = Flask(__name__)
+api = Api(app)
 
-# Sample data - Quotes with associated keywords and authors
 quotes_data = [
     {"quote": "Happiness is not something ready made. It comes from your own actions.",
         "author": "Dalai Lama", "keywords": ["happy"]},
@@ -14,16 +18,16 @@ quotes_data = [
         "author": "Franklin D. Roosevelt", "keywords": ["fear"]},
     {"quote": "Sadness flies away on the wings of time.",
         "author": "Jean de La Fontaine", "keywords": ["sad"]},
-    # Add more quotes here
 ]
-
 
 @app.route('/')
 def index():
+    random_quote = random.choice(quotes_data)
     unique_keywords = set()
     for quote in quotes_data:
         unique_keywords.update(quote['keywords'])
-    return render_template('index.html', quotes=quotes_data, keywords=unique_keywords)
+
+    return render_template('index.html', quotes=quotes_data, random_quote=random_quote, keywords=unique_keywords)
 
 
 @app.route('/keyword/<keyword>')
@@ -39,6 +43,36 @@ def author_quotes(author):
         quote for quote in quotes_data if author.lower() == quote['author'].lower()]
     return render_template('author_quotes.html', author=author, quotes=filtered_quotes)
 
+
+class QuoteList(Resource):
+    def get(self):
+        return jsonify(quotes_data)
+
+
+class RandomQuote(Resource):
+    def get(self):
+        quote = random.choice(quotes_data)
+        return jsonify(quote)
+
+
+class KeywordQuotes(Resource):
+    def get(self, keyword):
+        filtered_quotes = [
+            quote for quote in quotes_data if keyword in quote['keywords']]
+        return jsonify(filtered_quotes)
+
+
+class AuthorQuotes(Resource):
+    def get(self, author):
+        filtered_quotes = [
+            quote for quote in quotes_data if author.lower() == quote['author'].lower()]
+        return jsonify(filtered_quotes)
+
+
+api.add_resource(QuoteList, '/api/quotes')
+api.add_resource(RandomQuote, '/api/random')
+api.add_resource(KeywordQuotes, '/api/keyword/<string:keyword>')
+api.add_resource(AuthorQuotes, '/api/author/<string:author>')
 
 if __name__ == '__main__':
     app.run(debug=True)
